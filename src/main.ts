cf602ef8982759af18b8c706b433dbe61d000c9a
James S. Bayley <36523314+jamesbayley@users.noreply.github.com>
2022-12-28 12:05:14 +0000

    Merging 'dev' into 'main'. (#1)
    * Added new font style.
    
    * Updated tsconfig.json.
    
    * Organised .gitignore file.
    
    * Switched to AGPL (v3) License. Removed 'rollup' and 'terser' dependencies; TypeScript compiler now generates a single output file.
    
    * Revised the 'quotes.json' data file.
    
    * Added 'watch' mode.
    
    * Updated 'Quote' type to reflect revised 'quotes.json' file.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,25 @@
-type Quote = [string, string];
+// Reflective quips and quotes.
+// Copyright (C) 2022 <James S. Bayley>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+interface Quote {
+  author: string;
+  quote: string;
+  source: string;
+  tags: string[];
+}
 
 void (async function main() {
   const host = document.location.origin;
@@ -8,7 +29,7 @@ void (async function main() {
   const quips: Quote[] = await res.json();
   const quipCount = quips.length;
   const index = Math.floor(Math.random() * (quipCount + 1));
-  const [author, quote] = quips[index];
+  const { author, quote } = quips[index];
 
   const container = document.querySelector("#container") as HTMLElement;
   const h1 = document.querySelector("h1") as HTMLElement;
